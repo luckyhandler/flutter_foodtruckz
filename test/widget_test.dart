9af5154d859c2383ac2b794e8b5b505ed59cda90
Nino Handler <ninohandler@gmail.com>
2023-04-10 20:13:04 +0200

    Add icon

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_foodtruckz/main.dart';
+import 'package:foodtruckz/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
